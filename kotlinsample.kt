34470d93c5e60b43c4640988cfb587e518adb723
Rahul Deshpande <rahul.deshpande92@gmail.com>
2024-12-31 15:43:05 +0400

    Remove 2 extension functions

--- kotlinsample.kt
+++ kotlinsample.kt
@@ -5,7 +5,3 @@ fun CharSequence?.safe(default: CharSequence = "") = this ?: default
 fun Int?.safe(default: Int = 0) = this ?: default
 
 fun Double?.safe(default: Double = 0.0) = this ?: default
-
-fun Int?.safe(default: Int = 0) = this ?: default
-
-fun Double?.safe(default: Double = 0.0) = this ?: default
