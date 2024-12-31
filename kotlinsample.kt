17432eba357171e5cc58f7b0f56d6a9483a191b8
Rahul Deshpande <rahul.deshpande92@gmail.com>
2024-12-31 15:39:48 +0400

    Add 2 new extension functions

--- kotlinsample.kt
+++ kotlinsample.kt
@@ -5,3 +5,7 @@ fun CharSequence?.safe(default: CharSequence = "") = this ?: default
 fun Int?.safe(default: Int = 0) = this ?: default
 
 fun Double?.safe(default: Double = 0.0) = this ?: default
+
+fun Int?.safe(default: Int = 0) = this ?: default
+
+fun Double?.safe(default: Double = 0.0) = this ?: default
